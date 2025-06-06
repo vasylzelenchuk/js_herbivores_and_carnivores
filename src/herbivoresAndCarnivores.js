d6eed0cd050898da84810dfeeb8510e35354ab8d
vasylzelenchuk <vasyazyasinya@gmail.com>
2025-06-06 21:25:02 +0200

    new  Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -5,7 +5,7 @@ class Animal {
 
   constructor(name, health) {
     this.name = name;
-    this.health = health || 100;
+    this.health = health !== undefined ? health : 100;
 
     Animal.alive.push(this);
   }
@@ -16,9 +16,9 @@ class Animal {
 }
 
 class Herbivore extends Animal {
-  constructor(name) {
+  constructor(name, health) {
     super(name);
-    this.health = this.health || 100;
+    this.health = health !== undefined ? health : 100;
     this.hidden = false;
   }
 
